0416c89c2a959eaf227c97850eb93bbc98a9863e
Yong En Lee <yongenlee@Yongs-MacBook-Pro.local>
2016-09-15 16:57:10 +0800

    facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ActiveRecord::Base
   has_many :authentications, :dependent => :destroy
 
   def self.create_with_auth_and_hash(authentication,auth_hash)
-    byebug
+
     create! do |u|
 
-      u.encrypted_password = (0...8).map { (65 + rand(26)).chr }.join
-      u.first_name = auth_hash["info"]["first_name"]
+      u.password = SecureRandom.base64(8)
+      u.first_name = auth_hash["info"]["name"].split[0]
+      u.last_name = auth_hash["info"]["name"].split[-1]
       u.email = auth_hash["extra"]["raw_info"]["email"]
       u.authentications<<(authentication)
     end
