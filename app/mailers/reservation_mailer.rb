71543f8e1ba9930ed5fb13c4ce7ecf518e5da633
Yong En Lee <yongenlee@Yongs-MacBook-Pro.local>
2016-09-25 22:12:56 +0800

    added single mailer

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,9 +1,20 @@
 class ReservationMailer < ApplicationMailer
-  def booking_email(customer, host, reservation_id)
-
+  def booking_emailhost(customer, host, reservation_id)
     @customer = customer
     @reservation_id = reservation_id
     @host = host
+    byebug
     mail(to: host.email, subject: 'You have received a new reservation!')
   end
+
+  # def booking_emailcustomer(customer, host, reservation_id, start_date, end_date)
+  #   @customer = customer
+  #   @reservation_id = reservation_id
+  #   @host = host
+  #   @start_date = start_date
+  #   @end_date = end_date
+
+  #   mail(to: customer.email, subject: "Your reservation has been received, and is awaiting the host's confirmation.")
+  # end
+
 end
