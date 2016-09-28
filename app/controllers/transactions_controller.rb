4569703ed0feb8e369d795e3d4bda18040212eba
Yong En Lee <yongenlee@Yongs-MacBook-Pro.local>
2016-09-28 11:09:48 +0800

    removed background job as cannot support that many worker

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -17,8 +17,8 @@ class TransactionsController < ApplicationController
     if @result.success?
       @reservation.save
       # ReservationJob.perform_later(@reservation.user, @listing.user, @reservation.id)
-      ReservationMailer.booking_emailcustomer(@reservation.user, @listing.user, @reservation.id).deliver_later
-      ReservationMailer.booking_emailhost(@reservation.user, @listing.user, @reservation.id).deliver_later
+      ReservationMailer.booking_emailcustomer(@reservation.user, @listing.user, @reservation.id).deliver_now
+      ReservationMailer.booking_emailhost(@reservation.user, @listing.user, @reservation.id).deliver_now
       redirect_to @listing, notice: "Congraulations! Your payment has been received and booking has been confirmed!"
     else
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
